5c084e073c00b571b5ab612d101da7f7d2b7ac06
sobop <88311796+sobop@users.noreply.github.com>
2022-05-23 16:52:46 +0900

    배너 디자인 수정

--- pa_bi_web/src/components/common/Banner/Banner.jsx
+++ pa_bi_web/src/components/common/Banner/Banner.jsx
@@ -1,5 +1,21 @@
-import { Ho } from "./Banner.style";
-
+import { Image, Text, Boxes, Box, Icon, TextBox, Main } from "./Banner.style";
+import Logo from "./logo.png";
 export default function Banner() {
-  return <Ho>팔고 비우고</Ho>;
+  return (
+    <Image>
+      <Main>
+        <img src={Logo} alt="Pabi" />
+        <TextBox>
+          <Text>팔고 비우고</Text>
+          <Text>중고 경매 플랫폼 파비를 만나보세요</Text>
+        </TextBox>
+      </Main>
+      <Boxes>
+        <Box>게시글 작성</Box>
+        <Box>진행형 매물</Box>
+        <Box>임박 매물</Box>
+        <Box>인기 매물</Box>
+      </Boxes>
+    </Image>
+  );
 }
--- pa_bi_web/src/components/common/Banner/Banner.style.js
+++ pa_bi_web/src/components/common/Banner/Banner.style.js
@@ -1,10 +1,63 @@
 import styled from "styled-components";
+import img from "./Img.png";
+import logo from "./logo.png";
 
-export const Ho = styled.div`
-  width: 1302px;
+export const Image = styled.div`
   height: 548px;
-  background-image: url(http://www.easynlight.com/wp-content/uploads/2018/09/gs%EC%9D%B8%EC%A6%9D%EB%A1%9C%EA%B3%A0.png);
-  background-size: contain;
+  background-image: url(${img});
+  background-blend-mode: multiply;
+  background-size: cover;
   margin: 0 auto 176px;
+  background-color: rgba(20, 20, 67, 0.7);
+  background-repeat: no-repeat;
+`;
+export const Main = styled.div`
+  text-align: center;
+`;
+export const TextBox = styled.div`
+  margin-left: 20px;
+  display: inline-block;
+`;
+export const Text = styled.div`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+  color: #ffffff;
+  &:first-child {
+    padding-top: 130px;
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 95px;
+  }
+`;
+export const Boxes = styled.div`
+  display: flex;
+  margin-top: 50px;
+  justify-content: center;
+`;
+export const Box = styled.div`
+  cursor: pointer;
+  width: 180px;
+  height: 55px;
   border-radius: 10px;
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  padding: 13px 0px;
+  background: #f1f1f5;
+  margin-right: 20px;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    background: #5252ff;
+    box-shadow: 0px 4px 13px rgba(0, 0, 0, 0.25);
+    color: #ffffff;
+    transition-duration: 0.3s;
+  }
+  &:active {
+    background: #09098f;
+    transition-duration: 0.1s;
+  }
 `;
