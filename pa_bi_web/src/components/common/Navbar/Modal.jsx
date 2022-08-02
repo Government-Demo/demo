d6f6550b9cca89354fddef60086aef1da5ac4cf4
sobop <88311796+sobop@users.noreply.github.com>
2022-08-02 23:32:38 +0900

    모달창 회원가입 추가

--- pa_bi_web/src/components/common/Navbar/Modal.jsx
+++ pa_bi_web/src/components/common/Navbar/Modal.jsx
@@ -4,6 +4,86 @@ import styled from "styled-components";
 import pa from "./Logo.png";
 import { useNavigate } from "react-router-dom";
 
+export default function Example() {
+  let navigate = useNavigate();
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  let [key, setKey] = useState(Login);
+
+  function Login() {
+    return (
+      <StyledModalBody he="310px">
+        <Input type={"email"} placeholder={"아이디를 입력해주세요"} />
+        <Input type={"password"} placeholder={"비밀번호를 입력해주세요"} />
+        <Box>
+          <Remember type={"checkbox"} />
+          <RememberLabel>로그인 상태 유지</RememberLabel>
+          <Find
+            onClick={() => {
+              handleClose();
+              navigate("/");
+            }}
+          >
+            아이디/비밀번호 찾기
+          </Find>
+        </Box>
+        <Button type={"submit"} value={"로그인"} />
+        <Close>
+          회원가입이 필요없으신가요?{" "}
+          {
+            <span
+              onClick={() => {
+                handleClose();
+              }}
+              style={{ color: "#000000", cursor: "pointer" }}
+            >
+              둘러보기 ＞
+            </span>
+          }
+        </Close>
+      </StyledModalBody>
+    );
+  }
+  function Register() {
+    return (
+      <StyledModalBody he="470px">
+        <Title>이메일</Title>
+        <Input type={"email"} placeholder={"이메일 주소를 입력해주세요"} />
+        <Title>비밀번호</Title>
+        <Input type={"password"} placeholder={"비밀번호"} />
+        <Title>비밀번호 확인</Title>
+        <Input type={"password"} placeholder={"비밀번호 확인"} />
+        <Terms co="#767676">
+          회원으로 가입하면 서비스 약관과 개인정보 처리방침을 읽고 이해한 것으로
+          간주됩니다.
+        </Terms>
+        <Button type={"submit"} value={"회원가입"} />
+      </StyledModalBody>
+    );
+  }
+  return (
+    <>
+      <Sign onClick={handleShow}>로그인/회원가입</Sign>
+
+      <StyledModal show={show} onHide={handleClose} centered>
+        <StyledModalHeader closeButton>
+          <StyledModalTitle>
+            <img src={pa} alt="logo" />에 로그인
+          </StyledModalTitle>
+          <Tabs>
+            <Tab onClick={() => setKey(Login)}>로그인</Tab>
+            <Tab onClick={() => setKey(Register)}>회원가입</Tab>
+          </Tabs>
+        </StyledModalHeader>
+        {key}
+      </StyledModal>
+    </>
+  );
+}
+
 const Sign = styled.div`
   cursor: pointer;
   font-weight: 700;
@@ -14,13 +94,11 @@ const Sign = styled.div`
 `;
 const StyledModal = styled(Modal)``;
 const StyledModalHeader = styled(Modal.Header)`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #dbdbdb;
   height: 150px;
-
-  div {
-    font-weight: 500;
-    font-size: 20px;
-    margin: 0 auto;
-  }
+  padding: 30px 65px 0;
   button {
     position: absolute;
     top: 20px;
@@ -28,15 +106,32 @@ const StyledModalHeader = styled(Modal.Header)`
   }
 `;
 const StyledModalTitle = styled(Modal.Title)`
+  font-weight: 500;
+  font-size: 20px;
   img {
     margin-right: 10px;
   }
 `;
+const Tabs = styled.div`
+  margin-top: auto;
+  margin-right: auto;
+  display: flex;
+`;
+const Tab = styled.div`
+  cursor: pointer;
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  padding: 15px 0;
+  border-bottom: 3px solid #0000d8;
+  &:first-child {
+    margin-right: 10px;
+  }
+`;
 const StyledModalBody = styled(Modal.Body)`
-  height: 310px;
+  height: ${(props) => props.he};
   padding: 0 65px;
   text-align: center;
-  margin-top: 25px;
 `;
 const Input = styled.input`
   padding: 8px 16px;
@@ -48,6 +143,7 @@ const Input = styled.input`
   border: 1px solid #c4c4c4;
   border-radius: 5px;
   &:first-child {
+    margin-top: 25px;
     margin-bottom: 20px;
   }
 `;
@@ -60,13 +156,14 @@ const Button = styled.input`
   margin-top: 30px;
   width: 365px;
   height: 35px;
-  background: #5252ff;
+  background: #0000d8;
   border: none;
   border-radius: 3px;
   color: #ffffff;
 `;
 const Remember = styled.input``;
 const RememberLabel = styled.label`
+  margin-left: 10px;
   font-weight: 400;
   font-size: 12px;
 `;
@@ -82,56 +179,15 @@ const Close = styled.div`
   margin-top: 30px;
   color: #767676;
 `;
-
-export default function Example() {
-  let navigate = useNavigate();
-
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Sign onClick={handleShow}>로그인/회원가입</Sign>
-
-      <StyledModal show={show} onHide={handleClose} centered>
-        <StyledModalHeader closeButton>
-          <StyledModalTitle>
-            <img src={pa} alt="logo" />에 로그인
-          </StyledModalTitle>
-        </StyledModalHeader>
-
-        <StyledModalBody>
-          <Input type={"email"} placeholder={"아이디를 입력해주세요"} />
-          <Input type={"password"} placeholder={"비밀번호를 입력해주세요"} />
-          <Box>
-            <Remember type={"checkbox"} />
-            <RememberLabel>로그인 상태 유지</RememberLabel>
-            <Find
-              onClick={() => {
-                handleClose();
-                navigate("/");
-              }}
-            >
-              아이디/비밀번호 찾기
-            </Find>
-          </Box>
-          <Button type={"submit"} value={"로그인"} />
-          <Close>
-            회원가입이 필요없으신가요?{" "}
-            {
-              <span
-                onClick={() => {
-                  handleClose();
-                }}
-                style={{ color: "#000000", cursor: "pointer" }}
-              >
-                둘러보기 ＞
-              </span>
-            }
-          </Close>
-        </StyledModalBody>
-      </StyledModal>
-    </>
-  );
-}
+const Title = styled.div`
+  display: flex;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+const Terms = styled.div`
+  color: ${(props) => props.co};
+  margin-top: 50px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 18px;
+`;
